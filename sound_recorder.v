70392db60eb035548613787f43b38c61e9beb845
sykwer <sykwer@gmail.com>
2019-05-28 04:02:57 +0900

    Use embedded ram

--- sound_recorder.v
+++ sound_recorder.v
@@ -3,9 +3,9 @@ module sound_recorder(
   read_data, write_pointer,
   BUSY, AD7673_DATA, CNVST_N,
 );
-  parameter SOUND_SAMPLING_RATE = 44100; // 44.1KHz
-  parameter SAMPLING_DURATION = 10; // 10sec
-  parameter MEMORY_SIZE = SOUND_SAMPLING_RATE * SAMPLING_DURATION;
+  // parameter SOUND_SAMPLING_RATE = 44100; // 44.1KHz
+  // parameter SAMPLING_DURATION = 10; // 10sec
+  parameter MEMORY_SIZE = 65536;
 
   // occilator: 125M clk/sec
   // sound sampling rate: 44.1KHz
@@ -16,21 +16,33 @@ module sound_recorder(
   input clk;
   input reset_n_clk;
   input record_n;
-  input [18:0] read_pointer;
+  input [15:0] read_pointer;
   output [9:0] read_data;
-  output reg [18:0] write_pointer;
+  output reg [15:0] write_pointer;
 
   // AD7673 IO
   input BUSY; // active when AD converting in progress
   input [17:0] AD7673_DATA; // AD converted data
   output reg CNVST_N; // activate negatively when start converting
 
-  reg [9:0] memory [0:MEMORY_SIZE-1];
+  reg write_enable_n;
   reg [31:0] sampling_counter;
+
+  wire [15:0] addr2mem;
+  wire [9:0] mem2out;
+
+  memory memory(addr2mem, clk, AD7673_DATA[9:0], write_enable_n, mem2out);
 
   // negedge BUSY : signal of finished conversion
   always @(posedge clk or negedge reset_n_clk or negedge BUSY) begin
     if (clk) begin // explicit syncronous circuit description is needed
+      if (!write_enable_n) begin
+        write_enable_n <= 1;
+        if (write_pointer < MEMORY_SIZE) begin
+          write_pointer <= write_pointer + 1;
+        end
+      end
+
       if (!reset_n_clk) begin
         CNVST_N <= 1;
         write_pointer <= 0;
@@ -38,11 +50,7 @@ module sound_recorder(
       end
       else if (!BUSY && !CNVST_N) begin
         CNVST_N <= 1;
-
-        if (write_pointer < MEMORY_SIZE) begin
-          memory[write_pointer] <= AD7673_DATA[9:0];
-          write_pointer <= write_pointer + 1;
-        end
+        write_enable_n <= 0;
       end
       else if (!record_n) begin
         if (sampling_counter >= SAMPLE_INTERVAL_CLK && !BUSY) begin
@@ -56,5 +64,6 @@ module sound_recorder(
     end
   end
 
-  assign read_data = (read_pointer < write_pointer) ? memory[read_pointer] : 10'bZ;
+  assign read_data = (read_pointer < write_pointer) ? mem2out : 10'bZ;
+  assign addr2mem = !record_n ? write_pointer : read_pointer;
 endmodule

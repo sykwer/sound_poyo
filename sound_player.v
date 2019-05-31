6956cc0b1f68e8f6fa08c02bf19a262cde2fb351
sykwer <sykwer@gmail.com>
2019-05-31 14:08:17 +0900

    Reformat

--- sound_player.v
+++ sound_player.v
@@ -9,30 +9,29 @@ module sound_player(
 
   // analog port IO
   output [9:0] ANALOG_PORT;
-  
+
   // for debug;
   input clk;
   input reset_n_clk;
-  
+
   reg [9:0] out;
   reg [31:0] cnt;
-  
+
   always @(posedge clk or negedge reset_n_clk) begin
     if (!reset_n_clk) begin
-	   out <= 0;
-		cnt <= 0;
-	 end
-	 else begin
-	   if (cnt >= 125000) begin
+	    out <= 0;
 		  cnt <= 0;
-		  out <= out + 1;
-		end
-		else begin
-		  cnt <= cnt + 1;
-		end
-	 end
+	  end
+	  else begin
+	    if (cnt >= 125000) begin
+		    cnt <= 0;
+		    out <= out + 1;
+		  end
+		  else begin
+		    cnt <= cnt + 1;
+		  end
+	  end
   end
-  
 
   // assign ANALOG_PORT = !sound_enable_n ? read_data : 10'd0;
   assign ANALOG_PORT = out;

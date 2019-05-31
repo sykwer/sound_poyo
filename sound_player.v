947db0221eb370ee6c0e0ddae30ba11322023293
sykwer <sykwer@example.com>
2019-05-31 12:51:39 +0900

    Fix

--- sound_player.v
+++ sound_player.v
@@ -1,6 +1,7 @@
 module sound_player(
   read_data, sound_enable_n,
   ANALOG_PORT,
+  clk, reset_n_clk, // for debug
 );
   // parent module IO
   input [9:0] read_data;
@@ -8,6 +9,31 @@ module sound_player(
 
   // analog port IO
   output [9:0] ANALOG_PORT;
+  
+  // for debug;
+  input clk;
+  input reset_n_clk;
+  
+  reg [9:0] out;
+  reg [31:0] cnt;
+  
+  always @(posedge clk or negedge reset_n_clk) begin
+    if (!reset_n_clk) begin
+	   out <= 0;
+		cnt <= 0;
+	 end
+	 else begin
+	   if (cnt >= 125000) begin
+		  cnt <= 0;
+		  out <= out + 1;
+		end
+		else begin
+		  cnt <= cnt + 1;
+		end
+	 end
+  end
+  
 
-  assign ANALOG_PORT = !sound_enable_n ? read_data : 10'd0;
+  // assign ANALOG_PORT = !sound_enable_n ? read_data : 10'd0;
+  assign ANALOG_PORT = out;
 endmodule
